853fce5a53a78c712a7900b50ef0b34904f30a25
Oli Larkin <olilarkin@googlemail.com>
2019-01-24 22:24:56 +0000

    FaustDSP update

--- Examples/IPlugFaustDSP/config.h
+++ Examples/IPlugFaustDSP/config.h
@@ -35,7 +35,7 @@
 
 #define AAX_TYPE_IDS 'EFN1', 'EFN2'
 #define AAX_TYPE_IDS_AUDIOSUITE 'EFA1', 'EFA2'
-#define AAX_PLUG_MFR_STR "AcmeInc\nAcmeInc\nAcme"
+#define AAX_PLUG_MFR_STR "Acme"
 #define AAX_PLUG_NAME_STR "IPlugFaustDSP\nIPEF"
 #define AAX_PLUG_CATEGORY_STR "Effect"
 #define AAX_DOES_AUDIOSUITE 1
@@ -46,6 +46,7 @@
 #define APP_N_VECTOR_WAIT 0
 #define APP_MULT 1
 #define APP_COPY_AUV3 0
+#define APP_RESIZABLE 0
 #define APP_SIGNAL_VECTOR_SIZE 64
 
 #define ROBOTTO_FN "Roboto-Regular.ttf"
